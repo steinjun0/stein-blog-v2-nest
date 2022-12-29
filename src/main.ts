06bdcd1bdec0fc8cd6f8c2240428437f67758fce
steinjun0 <1142308@naver.com>
2022-12-29 02:48:20 +0000

    cors 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const whitelist: Array<string> = ['https://blog.steinjun.net', 'https://test.blog.steinjun.net']
+  if (process.env.NODE_ENV === 'development')
+    whitelist.push('http://localhost:3000')
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  });
   await app.listen(8888);
 }
 bootstrap();
