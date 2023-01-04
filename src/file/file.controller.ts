e22d75964243f480504c9670b0e63bd5ddf9c4fa
steinjun0 <1142308@naver.com>
2023-01-04 11:11:00 +0900

    file thumbnail image 함수 생성 - thumbnail 없을 시, 내부 파일중 하나를 전달 - service로 코드 분할

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,17 +1,34 @@
-import { Bind, Controller, Get, Param, Post, Res, StreamableFile, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Bind, Controller, Get, NotFoundException, Param, Post, Res, StreamableFile, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
-import { createReadStream, createWriteStream, existsSync, fstat, lstatSync, mkdirSync, rename, writeFileSync } from 'fs';
+import { createReadStream, createWriteStream, existsSync, fstat, lstatSync, mkdirSync, readdir, rename, writeFileSync } from 'fs';
 import { join } from 'path';
 import { diskStorage } from 'multer'
+import { FileService } from './file.service';
 
 
 
 @Controller('file')
 export class FileController {
+    constructor(private readonly fileService: FileService) { }
+
+    @Get('post/:postId/thumbnail')
+    getPostThumbnail(@Param('postId') postId: string, @Param('fileName') fileName: string): StreamableFile | NotFoundException {
+        const filePath = this.fileService.getThumbnailOrSubstitute(postId)
+
+        if (filePath === 'there is no file') {
+            throw new NotFoundException('there is no File')
+        } else {
+            console.log('filePath', filePath)
+            const file = createReadStream(join(process.cwd(), filePath));
+            return new StreamableFile(file)
+        }
+    }
+
     @Get('post/:postId/:fileName')
     getPostFile(@Param('postId') postId: string, @Param('fileName') fileName: string): StreamableFile {
         const file = createReadStream(join(process.cwd(), `post_files/${postId}/${fileName}`));
         return new StreamableFile(file);
+
     }
 
     @Post('post')
@@ -29,16 +46,8 @@ export class FileController {
             },
         })
     }))
+
     uploadPostFile(@UploadedFiles() files: Array<Express.Multer.File>) {
-        const response = [];
-
-        files.forEach(file => {
-            const fileReponse = {
-                filename: file.filename,
-            };
-            response.push(fileReponse);
-        });
-
-        return response;
+        return this.fileService.uploadPostFiles(files);
     }
 }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { existsSync, readdirSync } from 'fs';
 
 @Injectable()
-export class FileService {}
+export class FileService {
+    getThumbnailOrSubstitute(postId: string): string {
+        if (existsSync(`post_files/${postId}/`)) {
+            if (existsSync(`post_files/${postId}/thumbnail`)) {
+                return `post_files/${postId}/thumbnail`;
+            } else {
+                const files = readdirSync(`post_files/${postId}/`)
+                if (files && files.length > 0) {
+                    console.log(`${postId}: there is file`)
+                    return `post_files/${postId}/${files[0]}`;
+                } else {
+                    console.log(`${postId}: there is no file`)
+                    return 'there is no file'
+                }
+            }
+        } else {
+            console.log(`${postId}: there is no file`)
+            return 'there is no file'
+        }
+
+    }
+    uploadPostFiles(files: Array<Express.Multer.File>) {
+        const response = [];
+        files.forEach(file => {
+            const fileReponse = {
+                filename: file.filename,
+            };
+            response.push(fileReponse);
+        });
+        return response
+    }
+}
