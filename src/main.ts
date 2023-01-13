3b8ef639ca6e32b3aa6ed4d1d3490d6a576c90c6
steinjun0 <1142308@naver.com>
2023-01-13 19:18:09 +0900

    업로드 용량 제한 50MB증가

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -17,6 +18,8 @@ async function bootstrap() {
       }
     }
   });
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(8888);
 }
 bootstrap();
