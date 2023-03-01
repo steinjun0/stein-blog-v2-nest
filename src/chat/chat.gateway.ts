ff790e5d176ad2b54e14e5641df7955f06d62323
steinjun0 <1142308@naver.com>
2023-03-01 23:09:34 +0900

    add nickname and change broadcast logic

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,14 +1,43 @@
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, OnGatewayConnection, OnGatewayInit, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Socket } from 'dgram';
 
-@WebSocketGateway()
+function shuffle(array: Array<any>) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+
+@WebSocketGateway({ cors: true })
 export class ChatGateway implements OnGatewayInit, OnGatewayDisconnect, OnGatewayConnection {
-
   wsClients: Socket[] = [];
+  nicknames: string[] = [
+    '강철 남자', '강한 시금치', '망치의 신',
+    '착한 거미', '이상한 의사',
+  ]
+  constructor() {
+    shuffle(this.nicknames)
 
-  @SubscribeMessage('events')
+  }
+
+  @SubscribeMessage('message')
   handleEvent(@MessageBody() data: string, @ConnectedSocket() client: Socket): void {
-    this.broadcast('events', data)
+    const clientIndex = this.wsClients.indexOf(client)
+    for (let i = 0; i < this.wsClients.length; i++) {
+      this.wsClients[i].emit('message', { owner: this.nicknames[clientIndex], value: data })
+    }
   }
 
   broadcast(event, value: any) {
@@ -22,8 +51,8 @@ export class ChatGateway implements OnGatewayInit, OnGatewayDisconnect, OnGatewa
   }
 
   handleConnection(client: Socket, ...args: any[]) {
-    if (this.wsClients.length > 10) {
-      client.emit('events', '입장 인원을 초과했습니다')
+    if (this.wsClients.length > 5) {
+      client.emit('message', '입장 인원을 초과했습니다')
     } else {
       this.wsClients.push(client)
     }
